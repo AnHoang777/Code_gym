995f488188157238cff0df633ec85da7123bb455
AnHoang777 <anhoang210307@gmail.com>
2024-09-26 19:51:54 +0700

    init project

--- while/main.js
+++ while/main.js
@@ -1,10 +1,18 @@
-let a = prompt("nhập số vào đây: ");
-while(a < 20){
-    if(a % 2 === 0){
-    alert(a + " là số chẵn");
+let a;
+let b = true;
+
+while (b) {
+    a = prompt("Nhập số vào đây: ");
+    a = Number(a); // Chuyển đổi a thành số
+
+    if (a > 20) {
+        b = false; // Đặt b thành false để thoát khỏi vòng lặp
+        alert(a + " vượt quá giới hạn 20");
+    } else {
+        if (a % 2 === 0) {
+            alert(a + " là số chẵn");
+        } else {
+            alert(a + " không phải là số chẵn");
+        }
     }
-    if(a % 2 != 0){
-    alert(a + " không phải là số chẵn");   
-    }
-    break;
 }
